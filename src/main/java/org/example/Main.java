93da1ab05fdb4d6dde46db2446de6465c625830b
fmel025 <00018720@uca.edu.sv>
2023-03-12 15:23:03 -0600

    feat: Add some variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        int speed = 10;
+        System.out.println(speed);
+
+        String name = "Fernando Melendez";
+        System.out.println(name);
     }
 }
